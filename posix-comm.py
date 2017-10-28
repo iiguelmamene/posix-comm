1f7278e597c48324637e7350c8d4cb4aac013acf
iiguelmamene <iiguelmamene@users.noreply.github.com>
2017-10-28 16:00:03 -0700

    added comments to posix-comm.py

--- posix-comm.py
+++ posix-comm.py
@@ -14,14 +14,14 @@ from optparse import OptionParser
 class implementcomm:
     def __init__(self, file1, file2, line1, line2, line3, isunsorted):
 
-	if file1 == "-":
+	if file1 == "-": # read from stdin
 	   self.list_1 = sys.stdin.readlines()
 	else:
 	   f1 = open(file1, 'r')
 	   self.list_1 = f1.readlines()
 	   f1.close()
 
-	if file2 == "-":
+	if file2 == "-": # read from stdin
 	   self.list_2 = sys.stdin.readlines()
 	else:
 	   f2 = open(file2, 'r')
@@ -30,7 +30,7 @@ class implementcomm:
 	
         self.results_list = []	
 
-	if isunsorted:
+	if isunsorted: # algorithm for unsorted files
 	   for x in self.list_1[:]:
 	      if x in self.list_2:
                  self.results_list.append("\t\t" + x)
@@ -40,7 +40,7 @@ class implementcomm:
            for y in self.list_2[:]:
               self.results_list.append("\t" + y)
              
-        else:
+        else: # algorithm for sorted files
            while (self.list_1 and self.list_2):
               if self.list_1[0] == self.list_2[0]:
                  self.results_list.append("\t\t" + self.list_1[0])
@@ -59,14 +59,14 @@ class implementcomm:
               for i in self.list_1[:]:
                  self.results_list.append(i)
         
-        if line1 and line2:
+        if line1 and line2: # user only wants to print column 3
            self.results_list[:] = [ x for x in self.results_list if "\t\t" in x ]
            x = len(self.results_list) - 1
            while (x >= 0):
               if "\t\t" in self.results_list[x]:
                  self.results_list[x]  = self.results_list[x].replace("\t\t", "", 1)
               x = x - 1
-        elif line1 and line3:
+        elif line1 and line3: # user only wants to print column 2
            x = len(self.results_list) - 1
            while (x >= 0):
               if "\t\t" in self.results_list[x]:
@@ -78,28 +78,28 @@ class implementcomm:
               if "\t" in self.results_list[x]:
                  self.results_list[x]  = self.results_list[x].replace("\t", "", 1)
               x = x - 1
-        elif line2 and line3:
+        elif line2 and line3: # user only wants to print column 1
            self.results_list[:] = [ x for x in self.results_list if "\t" not in x ]     
-        elif line1:
+        elif line1: # user only wants to print columns 2 and 3
            self.results_list[:] = [ x for x in self.results_list if "\t" in x ]
            x = len(self.results_list) - 1
            while (x >= 0):
               if "\t" in self.results_list[x]:
                  self.results_list[x]  = self.results_list[x].replace("\t", "", 1)   
               x = x - 1
-        elif line2:
+        elif line2: # user only wants to print columns 1 and 3
            self.results_list[:] = [ x for x in self.results_list if "\t\t" in x or ("\t" not in x) ]
            x = len(self.results_list) - 1
            while (x >= 0):
               if "\t" in self.results_list[x]:
                  self.results_list[x]  = self.results_list[x].replace("\t\t", "\t", 1)
               x = x - 1
-        elif line3:
+        elif line3: # user only wants to print columns 1 and 2
            self.results_list[:] = [ x for x in self.results_list if "\t\t" not in x ]
 
-        if line1 and line2 and line3:
+        if line1 and line2 and line3: # do not print anything if the user wants to suppress all three columns
            n = 0
-        else:
+        else: # print the desired columns
            for i in self.results_list[:]:
               sys.stdout.write(i)  
 def main():
@@ -109,6 +109,8 @@ def main():
 select or reject lines common to two files"""
 
     parser = OptionParser(version=version_msg, usage=usage_msg)
+
+    # add options -1, -2, -3, and -u
 
     parser.add_option("-1", action="store_true", dest="suppress_line1", default=False,
     help="Suppress printing of column 1.")
@@ -154,7 +156,7 @@ select or reject lines common to two files"""
     file_1 = args[0]
     file_2 = args[1]
     
-    if file_1 == "-":
+    if file_1 == "-": # both files cannot be read from stdin
        if file_2 == "-":
           parser.error("cannot read both files from stdin, as both files cannot be denoted by -")       
     
